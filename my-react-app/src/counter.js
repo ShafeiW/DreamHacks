5ecc2151a7fdde5c4de6dd5adc6ecd1b551c937e
ShafeiW <shafei.waqar@icloud.com>
2025-03-15 11:03:37 -0700

    lebron front end initial

--- my-react-app/src/counter.js
+++ my-react-app/src/counter.js
@@ -1,9 +1,24 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+  let clicked = false
+  
+  const setButtonText = (isClicked) => {
+    clicked = isClicked
+    if (clicked) {
+      element.innerHTML = `Channeling Lebron's wisdom...`
+      element.disabled = true
+      
+      // Simulate loading and then show a message
+      setTimeout(() => {
+        alert("The King's wisdom is coming soon! This feature is under development.")
+        element.innerHTML = `Get Started`
+        element.disabled = false
+        clicked = false
+      }, 2000)
+    } else {
+      element.innerHTML = `Get Started`
+    }
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  
+  element.addEventListener('click', () => setButtonText(true))
+  setButtonText(false)
 }
--- my-react-app/src/main.js
+++ my-react-app/src/main.js
@@ -1,24 +1,19 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+  <div class="lebron-container">
+    <div class="glow-effect"></div>
+    <h1>Lebron GPT</h1>
+    <p class="tagline">The wisdom of greatness, illuminated</p>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="start-button" type="button">Get Started</button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
+    <p class="description">
+      Experience the ethereal basketball intelligence of the King
     </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+// Repurpose the counter function for our start button
+setupCounter(document.querySelector('#start-button'))
